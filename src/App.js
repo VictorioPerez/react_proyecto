00eebf05e18fe4940461852bdb76f5cb42f26e29
Victorio Perez <vic.perez2682@gmail.com>
2022-06-07 22:24:01 -0300

    Clase 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NavBar from "./components/NavBar";  
+import ItemListContainer from "./components/ItemListContainer";
 function App(){
     return(
     <> 
-        <NavBar></NavBar>
+        <NavBar saludo = "hola"></NavBar>{/* Header */}
+        <ItemListContainer greeting={'Prueba item list'}/> 
         <h4>Hola mundo con JSX UWU</h4>
         <p>Prueba</p>
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,45 @@
-function NavBar(){
+import CartWidget from './CartWidget'
+
+function NavBar(props){
+    //console.log(props.saludo)
 return (
 <>
     <header>
-        
-        <nav className="contenedorNavbar">
-            <div className="menus">
-            <h1 className="tituloNavbar">Compra Vic</h1>
-                <li className="navbarPrueba">
-                    <a href="">
-                        Inicio
-                    </a>
-                </li>
-                <li className="navbarPrueba">
-                    <a href="">
-                        Catalogo
-                    </a>
-                </li>
-                <li className="navbarPrueba">
-                    <a href="">
-                        Contacto
-                    </a>
-                </li>
+        {/* <h3>{props.saludo}</h3> */}
+        {
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand mx-2" href="#">Compra Vic</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse px-5" id="navbarNavDropdown">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item ">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item dropdown mx-5">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Catalogo
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Componentes</a></li>
+                                <li><a class="dropdown-item" href="#">Perifericos</a></li>
+                                <li><a class="dropdown-item" href="#">Combos</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
+                        </li>
+
+                    </ul>
+                    <CartWidget/>
+                </div>
             </div>
-        </nav>
+        </nav>}
     </header>
 </>
 )
